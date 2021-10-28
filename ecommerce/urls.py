df43b652c224bc8784e74506cc15cc056917415f
Sruthy-M <sruthy.manithody@gmail.com>
2021-10-28 23:56:24 +0530

    make it better

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -21,7 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-  
     path('',include('store.urls'),name='app_store'),
     path('',include('accounts.urls'),name='auth'),
     path('',include('store.api.urls'),name='api'),
