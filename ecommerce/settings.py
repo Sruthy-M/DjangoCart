a8bff25b9ed532e268a0a9b32726b74cfcddd685
Sruthy-M <sruthy.manithody@gmail.com>
2021-11-15 18:47:38 +0530

    some files deleted

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'b_jk^-3-9x5@!xfc#ocq8^=9^2ie2-100uxr2hujt))a=&ni9u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
